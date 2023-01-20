2a18569f485f7a038da35cbe90b1c8ef9fea5627
ashharr <shaikh.ashhar6@gmail.com>
2023-01-21 02:35:27 +0530

    first commit

--- src/data/portfolio.js
+++ src/data/portfolio.js
@@ -2,19 +2,19 @@ export default [
    {
       title: 'Machine Learning Chatbot',
       imgUrl: '/assets/ml-chatbot.jpg',
-      stack: ['HTML', 'CSS', 'JavaScript'],
+      stack: ['Flask', 'Python', 'HTML','CSS'],
       link: 'https://console.dialogflow.com/api-client/demo/embedded/19719ab7-c51b-4cfd-a171-b05810891d3e',
    },
    {
       title: 'Ecommerce Store',
       imgUrl: '/assets/clothing-store.jpg',
-      stack: ['React', 'Tailwind', 'Vite'],
+      stack: ['React', 'Next', 'Stripe','Sanity'],
       link: 'https://ecommerce-sanity-stripe-b8b3.vercel.app/',
    },
    {
       title: 'Expense Tracker App',
       imgUrl: '/assets/expense-tracker.png',
       stack: ['React', 'HTML', 'CSS','JavaScript'],
-      link: 'https://youtu.be/A8eegxL82PI',
+      link: 'https://expense-tracker-ashharr.netlify.app/',
    },
 ]
